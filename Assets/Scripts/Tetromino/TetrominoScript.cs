cb463088ce06200b7c949c6ba92314153023ebbb
OE100 <omer.er10@gmail.com>
2024-08-24 19:17:18 +0300

    delegated the tetromino move operations to the board manager

--- Assets/Scripts/Tetromino/TetrominoScript.cs
+++ Assets/Scripts/Tetromino/TetrominoScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Management;
+using Management.Board;
 using UnityEngine;
 
 namespace Tetromino
@@ -7,14 +8,14 @@ namespace Tetromino
     public class TetrominoScript : MonoBehaviour
     {
         [SerializeField] private Color color;
-        
-        private readonly List<GameObject> _tetrominoBlocks = new();
 
-        public void RegisterBlock(GameObject block) => _tetrominoBlocks.Add(block);
+        public List<GameObject> TetrominoBlocks { get; } = new();
+
+        public void RegisterBlock(GameObject block) => TetrominoBlocks.Add(block);
 
         private void Start()
         {
-            foreach (var tetrominoBlock in _tetrominoBlocks)
+            foreach (var tetrominoBlock in TetrominoBlocks)
             {
                 tetrominoBlock.GetComponent<SpriteRenderer>().color = color;
             }
@@ -23,28 +24,22 @@ namespace Tetromino
 
         public void Move(Vector3 offset)
         {
-            transform.position += offset;
+            if (!BoardManager.Instance.TryMove(this, offset) && BoardManager.Instance.ShouldStop(this))
+                Destroy(this);
         }
 
         public void Rotate()
         {
-            // rotate all blocks around the center
-            var center = transform.position;
-            foreach (var tetrominoBlock in _tetrominoBlocks)
-            {
-                var block = tetrominoBlock.transform;
-                var offset = block.position - center;
-                var rotatedOffset = new Vector3(offset.y, -offset.x, center.z);
-                block.position = center + rotatedOffset;
-            }
+            BoardManager.Instance.TryRotate(this);
         }
 
         private void OnDestroy()
         {
-            foreach (var block in _tetrominoBlocks)
+            foreach (var block in TetrominoBlocks)
             {
                 block.transform.SetParent(null);
             }
+            BoardManager.Instance.PutOnBoard(TetrominoBlocks.ToArray());
             GameManager.Instance.CurrentTetrominos.Remove(this);
         }
     }
