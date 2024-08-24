5211ad01624c8989f78810e845476d3abe1c13cd
OE100 <omer.er10@gmail.com>
2024-08-24 19:17:45 +0300

    defined functions for moving and rotating tetrominos with validation.

--- Assets/Scripts/Management/Board/BoardManager.cs
+++ Assets/Scripts/Management/Board/BoardManager.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Management.Score;
+using Tetromino;
 using UnityEngine;
 using Utils;
 
@@ -20,7 +22,7 @@ namespace Management.Board
 
         private void CreateBoard()
         {
-            _board = new GameObject[width, height];
+            _board = new GameObject[width, height + 1];
             for (var x = 0; x < width; x++)
             for (var y = 0; y < height; y++)
                 _board[x, y] = null;
@@ -60,6 +62,122 @@ namespace Management.Board
             CreateBoard();
             CreateBorder();
             DirectCamera();
+        }
+
+        public (int, int) PositionToBoard(Vector3 p) => (Mathf.FloorToInt(p.x), Mathf.FloorToInt(p.y));
+
+        public bool IsEmptyIndex(int x, int y)
+        {
+            if (x >= width || x < 0 || y < 0 || y > height) return false;
+            return _board[x, y] == null;
+        }
+        
+        public bool TryMove(TetrominoScript mover, Vector3 direction)
+        {
+            var legal = true;
+            foreach (var block in mover.TetrominoBlocks)
+            {
+                var newPos = block.transform.position + direction;
+                var (x, y) = PositionToBoard(newPos);
+                legal &= IsEmptyIndex(x, y);
+            }
+
+            if (legal) Move(mover.transform, direction);
+            
+            return legal;
+        }
+
+        public bool TryRotate(TetrominoScript rotator)
+        {
+            var legal = true;
+            
+            var center = rotator.transform.position;
+            var length = rotator.TetrominoBlocks.Count;
+            var newPositions = new Vector3[length];
+            for (var i = 0; i < length; i++)
+            {
+                var block = rotator.TetrominoBlocks[i];
+                var tr = block.transform;
+                var offset = tr.position - center;
+                var rotatedOffset = new Vector3(offset.y, -offset.x, center.z);
+                newPositions[i] = center + rotatedOffset;
+                var (x, y) = PositionToBoard(newPositions[i]);
+                legal &= IsEmptyIndex(x, y);
+            }
+            
+            for (var i = 0; legal && i < length; i++)
+                rotator.TetrominoBlocks[i].transform.position = newPositions[i];
+            
+            return legal;
+        }
+
+        public void Move(Transform t, Vector3 offset) => t.position += offset;
+
+        public bool ShouldStop(TetrominoScript tetromino)
+        {
+            var stop = false;
+
+            foreach (var block in tetromino.TetrominoBlocks)
+            {
+                var (x, y) = PositionToBoard(block.transform.position);
+                stop |= !IsEmptyIndex(x, y - 1);
+            }
+            
+            return stop;
+        }
+
+        public void PutOnBoard(GameObject[] objects)
+        {
+            var scoreBonus = 0;
+            
+            foreach (var obj in objects)
+            {
+                // get the board position of the object
+                var (x, y) = PositionToBoard(obj.transform.position);
+                
+                // check for loss condition
+                if (y == height)
+                {
+                    // todo: we lost, do not add score and notify game manager
+                    return;
+                }
+                
+                // put the object on the board
+                if (IsEmptyIndex(x, y))
+                    _board[x, y] = obj;
+                
+                // check if line is now full
+                if (IsRowFull(y))
+                {
+                    scoreBonus += 1;
+                    ClearRow(y);
+                }
+            }
+            
+            // add score to current score
+            var currentLineBonus = 100;
+            var totalAdd = 0;
+            for (var i = 0; i < scoreBonus; i++)
+            {
+                totalAdd += currentLineBonus;
+                currentLineBonus += 100;
+            }
+            ScoreManager.Instance.AddScore(totalAdd);
+        }
+
+        private void ClearRow(int y)
+        {
+            // todo: clear the row, remove all blocks and drop all rows above one down
+        }
+
+        public bool IsRowFull(int y)
+        {
+            var full = true;
+
+            for (var x = 0; x < width; x++)
+                full &= _board[x, y] != null;
+            
+            return full;
         }
     }
 }
