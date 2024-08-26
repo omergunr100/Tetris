95bc50a0c46fc40f98383f37d20a85c5e9344d39
OE100 <omer.er10@gmail.com>
2024-08-27 01:00:00 +0300

    there is no state

--- Assets/Scripts/Tetromino/BlockScript.cs
+++ Assets/Scripts/Tetromino/BlockScript.cs
@@ -4,9 +4,5 @@ namespace Tetromino
 {
     public class BlockScript : MonoBehaviour
     {
-        private void Awake()
-        {
-            GetComponentInParent<TetrominoScript>().RegisterBlock(gameObject);
-        }
     }
 }
