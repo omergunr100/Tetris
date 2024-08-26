a156ceb8a5f65eb004b25474d44539248f63b992
OE100 <omer.er10@gmail.com>
2024-08-27 01:01:17 +0300

    created object pool

---
+++ Assets/Scripts/Pooling/TetrisObjectPool.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Pooling
+{
+    public class TetrisObjectPool<T> : IObjectPool<T> where T : MonoBehaviour
+    {
+        private readonly List<T> _pool = new();
+        
+        private readonly Func<T> _createObject;
+        private readonly Action<T> _onGet;
+        private readonly Action<T> _onRelease;
+        private readonly Action<T> _onDestroy;
+        private readonly int _maxSize;
+        
+        private int _currSize = 0;
+        
+        public TetrisObjectPool(Func<T> createObject,
+            Action<T> onGet = null, Action<T> onRelease = null, Action<T> onDestroy = null, 
+            int maxSize = 500, int initialSize = 0)
+        {
+            _createObject = createObject;
+            _onGet = onGet;
+            _onRelease = onRelease;
+            _onDestroy = onDestroy;
+            _maxSize = maxSize;
+
+            if (initialSize > 0)
+            {
+                var arr = new T[initialSize];
+                
+                for (var i = 0; i < initialSize; i++)
+                    arr[i] = Get();
+
+                for (var i = 0; i < initialSize; i++)
+                    Release(arr[i]);
+            }
+        }
+        
+        public T Get()
+        {
+            T obj;
+            
+            if (_pool.Count > 0)
+            {
+                obj = _pool[0];
+                _pool.RemoveAt(0);
+            }
+            else
+            {
+                obj = _createObject();
+            }
+            _onGet?.Invoke(obj);
+
+            return obj;
+        }
+
+        public void Release(T obj)
+        {
+            _onRelease?.Invoke(obj);
+            if (_maxSize >= _currSize + 1)
+            {
+                _pool.Add(obj);
+                _currSize++;
+            }
+            else
+            {
+                _onDestroy(obj);
+                Object.Destroy(obj);
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (var behaviour in _pool)
+            {
+                _onDestroy?.Invoke(behaviour);
+                Object.Destroy(behaviour);
+            }
+        }
+    }
+}
