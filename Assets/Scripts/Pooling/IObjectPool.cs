c978ae6e69f33f3e797448998acca39afcd640a5
OE100 <omer.er10@gmail.com>
2024-08-27 01:00:52 +0300

    defined object pool requirements

---
+++ Assets/Scripts/Pooling/IObjectPool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Pooling
+{
+    public interface IObjectPool<T> where T : MonoBehaviour
+    {
+        T Get();
+        void Release(T obj);
+        void Clear();
+    }
+}
