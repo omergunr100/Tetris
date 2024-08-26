0b63ae5f075588f6314568236ca7df834f730fe2
OE100 <omer.er10@gmail.com>
2024-08-27 01:02:19 +0300

    created abstraction for interaction with object pools

---
+++ Assets/Scripts/Management/Pooling/PoolStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Management.Prefabs;
+using Pooling;
+using Tetromino;
+using UnityEngine;
+using Utils;
+
+namespace Management.Pooling
+{
+    public class PoolStore : Singleton<PoolStore>
+    {
+        private readonly IObjectPool<BlockScript> _blockPool = new TetrisObjectPool<BlockScript>(
+            createObject: () => Instantiate(PrefabStore.Instance.blockPrefab),
+            initialSize: 100
+        );
+
+        private readonly Dictionary<Type, object> _pools = new();
+
+        private void Awake()
+        {
+            _pools.Add(typeof(BlockScript), _blockPool);
+        }
+
+        public T Get<T>() where T : MonoBehaviour => (_pools[typeof(T)] as IObjectPool<T>)?.Get();
+
+        public void Release<T>(T obj) where T : MonoBehaviour
+        {
+            if (_pools.TryGetValue(typeof(T), out var pool))
+                ((IObjectPool<T>) pool).Release(obj);
+            else
+                Destroy(obj);
+        }
+        
+        public void Clear<T>() where T : MonoBehaviour => (_pools[typeof(T)] as IObjectPool<T>)?.Clear();
+    }
+}
