0fc9477e42dfd51e2f6b5de77be62440ea13364f
OE100 <omer.er10@gmail.com>
2024-08-27 00:59:48 +0300

    defined block directive object for specifying relative co-ordinates of blocks

---
+++ Assets/Scripts/Tetromino/BlockDirective.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Tetromino
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Block Directive")]
+    public class BlockDirective : ScriptableObject
+    {
+        [SerializeField] public Vector3 relativePosition;
+        [SerializeField] public Quaternion rotation;
+    }
+}
