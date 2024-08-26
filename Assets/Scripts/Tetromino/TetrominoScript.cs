cf74adbe23d4c61c0cbc227cae8a7b516036aa7f
OE100 <omer.er10@gmail.com>
2024-08-27 01:02:57 +0300

    moved from life-cycle functions to regular functions for re-use

--- Assets/Scripts/Tetromino/TetrominoScript.cs
+++ Assets/Scripts/Tetromino/TetrominoScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Management.Board;
+using Management.Pooling;
 using UnityEngine;
 
 namespace Tetromino
@@ -7,39 +8,45 @@ namespace Tetromino
     public class TetrominoScript : MonoBehaviour
     {
         [SerializeField] private Color color;
+        [SerializeField] private List<BlockDirective> blockDirectives;
+            
+        private readonly List<BlockScript> _tetrominoBlocks = new();
 
-        public List<GameObject> TetrominoBlocks { get; } = new();
-
-        public void RegisterBlock(GameObject block) => TetrominoBlocks.Add(block);
-
-        private void Start()
+        public void Setup()
         {
-            foreach (var tetrominoBlock in TetrominoBlocks)
+            foreach (var directive in blockDirectives)
             {
-                tetrominoBlock.GetComponent<SpriteRenderer>().color = color;
+                var block = PoolStore.Instance.Get<BlockScript>();
+                block.GetComponent<SpriteRenderer>().color = color;
+                _tetrominoBlocks.Add(block);
+                block.transform.parent = transform;
+                block.transform.localPosition = directive.relativePosition;
+                block.transform.localRotation = directive.rotation;
             }
+            // todo: think about implementing tetromino script as singleton
+            // todo: instead of prefabs have a list of lists of block directives
             BoardManager.Instance.CurrentTetromino = this;
+        }
+
+        public void Remove()
+        {
+            BoardManager.Instance.CurrentTetromino = null;
+            foreach (var block in _tetrominoBlocks)
+            {
+                block.transform.SetParent(null);
+            }
+            BoardManager.Instance.PutOnBoard(_tetrominoBlocks);
         }
 
         public void Move(Vector3 offset)
         {
             if (!BoardManager.Instance.TryMove(this, offset) && BoardManager.Instance.ShouldStop(this))
-                Destroy(this);
+                Remove();
         }
 
         public void Rotate()
         {
             BoardManager.Instance.TryRotate(this);
-        }
-
-        private void OnDestroy()
-        {
-            BoardManager.Instance.CurrentTetromino = null;
-            foreach (var block in TetrominoBlocks)
-            {
-                block.transform.SetParent(null);
-            }
-            BoardManager.Instance.PutOnBoard(TetrominoBlocks.ToArray());
         }
     }
 }
