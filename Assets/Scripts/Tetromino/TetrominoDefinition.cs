d6d1b009bdd2d282463ae03ac27c60ca26fae16f
OE100 <omer.er10@gmail.com>
2024-08-27 17:42:57 +0300

    added a tetromino definition scriptable object as a building instruction object for tetrominos to replace prefabs and make tetromino script a singleton

---
+++ Assets/Scripts/Tetromino/TetrominoDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tetromino
+{
+    [CreateAssetMenu(menuName = "Scriptable Objects/Tetromino Definition")]
+    public class TetrominoDefinition : ScriptableObject
+    {
+        [SerializeField] public Color pieceColor;
+        [SerializeField] public List<BlockDirective> blockDirectives;
+    }
+}
